d064e992105fa8801feb8df8c531dc5b5610407f
goyalk130 <goyalkaran130@gmail.com>
2023-11-07 23:13:24 +0530

    added data

--- app/api/random/route.js
+++ app/api/random/route.js
@@ -1,6 +1,7 @@
+import data from "@/utils/data"
 import { NextResponse } from "next/server"
 
 export const GET = async (req,{params})=>{
-    console.log(params)
-    return NextResponse.json({success:true})
+    
+    return NextResponse.json({success:true,data:data})
 }
